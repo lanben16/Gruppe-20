b24f6f218bbe817012fd01c49a2e355a07c63ee2
Jonas Nordby <jonas_nordby@hotmail.com>
2017-05-11 11:28:14 +0200

    Update barer.php

--- barer.php
+++ barer.php
@@ -1,5 +1,4 @@
 
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,97 +9,80 @@
 <meta name="author" content="">
 <link href="barer.css" rel="stylesheet" type="text/css">
 <title>New HMTL document by NewJect</title>
-    
+
     <?php
-
 $port = 3306;
-
 $username = 'lanben16_gr20';
-
 $password = 'Ntcrvy&6])8k';
-
 $name = 'lanben16_Barer';
-
 // Create connection
 $connection = new PDO("mysql:host=tek.westerdals.no;dbname={$name};port={$port}", $username, $password);
-
-$statement = $connection->prepare('SELECT * FROM barer');
+$statement = $connection->prepare('SELECT * FROM barerVulk');
 $statement->execute();
-
 $barer = [];
-
 while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
     $barer[] = $row;
 }
-
 ?>
 
 </head>
-    
+
 <body>
-  
+
 <?php foreach ($barer as $bar) { ?>
-    
-<div id="myBtn"> 
-    <img class="thumbnailImg" src="<?= $bar['Image_path'] ?>">
-    <p class="thumbnailName"><?= $bar['Id_Navn'] ?></p>
-    
+
+<div class="barThumbnail">
+        <img class="thumbnailImg" src="<?= $bar['Image_path'] ?>">
+        <div class="thumbnailName">
+            <p ><?= $bar['Id_Navn'] ?></p>
+        </div>
 </div>
 
-<div id ="myModal" class="modal">
+<div class="modal">
     <div class="modalContent">
-        
-        
-        <img class="barImg" src="<?= $bar['Image_path'] ?>">
+        <div class="barImg">
+            <img  src="<?= $bar['Image_path'] ?>">
+        </div>
         <div class="close">&times;</div>
-        
-        
-            
-            
-            <p class="barName"><?= $bar['Id_Navn'] ?></p>
-            <ul class="barInfo">
-                <li>Adresse:<?= $bar['Adresse'] ?></li>
-                <li>Ølpris:<?= $bar['Pris'] ?></li>
-                <li>ID:<?= $bar['Id_Navn'] ?></li>
-                <li>Åpningtider:<?= $bar['Aapningstider_Mandag'] ?></li>
-            </ul>
-            
-            
-           
-        
+        <div class="barName">
+            <p><?= $bar['Id_Navn'] ?></p>
+        </div>
+        <ul class="barInfo">
+            <li>Adresse:<?= $bar['Adresse'] ?></li>
+            <li>Ølpris:<?= $bar['Pris'] ?></li>
+            <li>ID:<?= $bar['Aldersgrense'] ?>år</li>
+            <li>Åpningtider:<?= $bar['Aapningstider_Mandag'] ?></li>
+        </ul>
     </div>
-    </div>
+</div>
+<?php } ?>
+<script>
     
-  <?php } ?>
-    
-
-    <script>
-        var modal = document.getElementById('myModal');
-
-
-        var btn = document.getElementById("myBtn");
-
-
-        var span = document.getElementsByClassName("close")[0];
-
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}  
-    </script>
+    var modal = document.getElementsByClassName("modal")[0];
+    var btn= document.getElementsByClassName("barThumbnail")[0];
+    var span = document.getElementsByClassName("close")[0];
     
     
+    
+    
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    
+        console.log(document.getElementsByClassName("modal"))
+    
+        console.log(document.getElementsByClassName("barThumbnail"))
+        
+       
+    
+</script>
 </body>
 </html>
