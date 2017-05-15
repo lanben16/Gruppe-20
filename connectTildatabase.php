c852698588ebd188ce7e3eb0e8745ff6e48b4344
lanben16 <lanben16@student.westerdals.no>
2017-05-15 13:14:21 +0200

    Create connectTildatabase.php

---
+++ connectTildatabase.php
@@ -0,0 +1,14 @@
+<?php
+
+$port = 3306;
+    
+$username = 'lanben16_gr20';
+    
+$password = 'Ntcrvy&6])8k';
+    
+$name = 'lanben16_Barer';
+    
+// Create connection
+$connection = new PDO("mysql:host=tek.westerdals.no;dbname={$name};port={$port}", $username, $password);
+
+?>
