72d6cd785a82a7b79c8ebc59ade6d1164a7b3f08
lanben16 <lanben16@student.westerdals.no>
2017-05-15 13:15:30 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,25 @@
+
+
+
+            <div class="SocialWrap">
+                <a target="_blank" class="facebook" href="https://facebook.com/westerdalsosloact/">
+                <img alt="facebook" src="//login.create.net/images/icons/user/facebook_30x30.png"border=0></a>
+                
+                <a target="_blank" class="twitter" href="https://twitter.com/westerdalsact/">
+                <img alt="twitter" src="//login.create.net/images/icons/user/twitter-b_30x30.png"border=0>
+                </a>
+                
+                <a target="_blank" class="instagram" href="https://instagram.com/westerdalsact/">
+                <img alt="instagram" src="https://c866088.ssl.cf3.rackcdn.com/assets/instagram30x30.png" border=0>
+                </a>
+                
+    </div>
+    
+    <p class="telefon">Telefon: 22 05 75 50</p>
+    <p class="epost">Epost: post@westerdals.no</p>
+    
+    <img class="FootLogo" src="WACT_hovedlogo_farge_rgb.png">
+    
+                
+</body>
+</html>
