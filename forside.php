e1441cf102f199e8cd1e140ea501ea95798060d7
lanben16 <lanben16@student.westerdals.no>
2017-05-28 15:01:09 +0200

    Update forside.php

--- forside.php
+++ forside.php
@@ -7,7 +7,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link rel="stylesheet" type="text/css" href="stylesheet.css">
-    
+    <!--Link til google font 'Lato'-->
 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 
     
@@ -19,41 +19,43 @@
     
 
 
-<div class="bakgrunn">
-   <img class="slideshow" src="Westerdals.jpg">
+<!--Bildebakgrunn-->
+   <img class="slideshow" src="Bilder/Slideshow/Westerdals.jpg">
     
     
-   <img class="slideshow" src="12992738_10156864448095085_469207469_n-960x720.jpg">
+   <img class="slideshow" src="Bilder/Slideshow/Fasade-fra-gaten.jpg">
     
-   <img class="slideshow" src="Fasade-fra-gaten.jpg">
+   <img class="slideshow" src="Bilder/Slideshow/Brenneriveien-9-4.jpg">
     
-   <img class="slideshow" src="N4A1603-800x600.jpg">
-</div>
-    
+   <img class="slideshow" src="Bilder/Slideshow/N4A2428-800x600.jpg">
+
+    <!--Titel på siden -->
     <div class="titleBakgrunn">
     <h2 id="Westerdals">WestStudent</h2>
     <p id="ACT">Nettside for studenter ved WOACT</p>
     </div>
+    
+    <!--Meny over campuser -->
     <div class="HjemMeny"> </div>
     <div id="CampusMeny">
     <p id="Campuser">Se våre <br> campuser!</p>
 
     <div class="menyFjerdingen">
     
-    <a id="Fjerdingen" href="CampusFjerdingen.php">Fjerdingen</a>
+    <a id="CampFjerdingen" href="CampusFjerdingen.php">Fjerdingen</a>
     </div>
     
     <div class="menyVulkan">
-    <a id="Vulkan" href="CampusVulkan.php">Vulkan</a>  
+    <a id="CampVulkan" href="CampusVulkan.php">Vulkan</a>  
     </div>
     
     <div class="menyBrenneriveien">
-    <a id="Brenneriveien" href="CampusBrenneriveien.php">Brenneriveien</a>
+    <a id="CampBrenneriveien" href="CampusBrenneriveien.php">Brenneriveien</a>
     </div>
         </div>
 
     
-    
+    <!-- Javascript-->
     
     <script>
     
@@ -69,19 +71,20 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 4000); // Change image every 4 seconds
+    setTimeout(carousel, 4000); // Bytter bilde hvert 4 sekund
 }    
     
 </script>
-    
+ 
+    <!--Logo i topp høyre hjørnet-->
 <div>
-<img class="logo" src="westerdals-logo1.png">
+<img class="logo" src="Bilder/Logoer/westerdals-logo1.png">
     </div>
-<div id="strek"></div>
     
+    <!--Footer-->
 <div class="fooT">
  <?php require 'footer.php' ?>
-    
  </div>
+    
 </body>
 </html>
