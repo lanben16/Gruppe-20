b04dcf80bb5c6b20c3a33ff72589ff0ef288f271
lanben16 <lanben16@student.westerdals.no>
2017-05-28 14:54:10 +0200

    Update CampusBrenneriveien.php

--- CampusBrenneriveien.php
+++ CampusBrenneriveien.php
@@ -5,8 +5,8 @@
     
 <meta charset="utf-8"> 
     
-<link type=text/css rel=stylesheet href="main.css">
-    
+<link type=text/css rel=stylesheet href="stylesheet.css">
+    <!--Link til googlefont 'Lato' -->
 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 
 
@@ -14,7 +14,18 @@
 </head>
     
 <body>
-             
+    <!--Bildebakgrunn med blur-->
+<div class="CampusBakgrunn">
+     <img class="CampusSlideshow" src="Bilder/Slideshow/Westerdals.jpg">
+    
+    
+   <img class="CampusSlideshow" src="Bilder/Slideshow/Fasade-fra-gaten.jpg">
+    
+   <img class="CampusSlideshow" src="Bilder/Slideshow/Brenneriveien-9-4.jpg">
+    
+   <img class="CampusSlideshow" src="Bilder/Slideshow/N4A2428-800x600.jpg">
+</div>  
+             <!--Container for tekstomeråde -->
 <div class="container">
     
 <div class="textbox">
@@ -27,29 +38,25 @@
     </div>
     </div>
 
-    
+    <!--Container for campusbilder-->
 <div class="slideshow-container">
 <div class="mySlides fade">
-<div class="numbertext">1 / 4</div>
-<img src="Bilder/FW_Brenneriveien_7-800x600.jpg" style="width:100%">
+<img src="Bilder/Brenneriveien/FW_Brenneriveien_7-800x600.jpg" style="width:100%">
 <div class="text">Brenneriveien ute</div>
 </div>
 
 <div class="mySlides fade">
-<div class="numbertext">2 / 4</div>
-<img src="Bilder/HG_brenneriveien1-800x600.jpg" style="width:100%">
+<img src="Bilder/Brenneriveien/HG_brenneriveien1-800x600.jpg" style="width:100%">
 <div class="text">Akerselven</div>
 </div>
 
   <div class="mySlides fade">
- <div class="numbertext">3 / 4</div>
-    <img src="Bilder/MH-brenneriveien-vulkan-oktober-5-800x600.jpg" style="width:100%">
+    <img src="Bilder/Brenneriveien/MH-brenneriveien-vulkan-oktober-5-800x600.jpg" style="width:100%">
     <div class="text">Kontoret</div>
   </div>
 
 <div class="mySlides fade">
- <div class="numbertext">4 / 4</div>
-    <img src="Bilder/MH-brenneriveien-vulkan-oktober-7-2-800x600.jpg" style="width:100%">
+    <img src="Bilder/Brenneriveien/MH-brenneriveien-vulkan-oktober-7-2-800x600.jpg" style="width:100%">
     <div class="text">Fremtiden</div>
   </div>
 
@@ -58,14 +65,26 @@
   <a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
 <br>
-
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
-</div>
-    
+    <!--JavaScript-->
 <script> 
+    //Javascript til bildebakgrunn
+     var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("CampusSlideshow");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    
+    slideIndex++;
+    
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 3000); // Bilde byttes hvert 3 sekund
+}  
+    //Javascript for campusbilder
 var slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
@@ -90,7 +109,7 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 </script>
-    
+    <!--Meny over campuser -->
 <div class="svartbox">
     <p class="Campuser">Våre campuser:</p>
 
@@ -105,11 +124,11 @@ function showSlides(n) {
     
     <div class="menyBrenneriveien">
     <a id="Brenneriveien" href="CampusBrenneriveien.php" style="color:gray;">Brenneriveien</a>
-        </div>
+        </div> <!-- Siden som brukeren er inne på blir markert med grå skrift -->
 </div>
-    
+    <!--Logo med link tilbake til forsiden -->
 <a  href="forside.php">
-     <img class="logo" src="westerdals-logo1.png">
+     <img class="logo" src="Bilder/Logoer/westerdals-logo1.png">
     </a>
     <p class="Hjem">Hjem</p>
     
