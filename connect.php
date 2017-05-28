043457a380bb83eab14e9309895a861f98b38e5e
lanben16 <lanben16@student.westerdals.no>
2017-05-28 14:57:47 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-
+/* Lager variabler med login info */
 $port = 3306;
     
 $username = 'lanben16_gr20';
@@ -8,7 +8,7 @@ $password = 'Ntcrvy&6])8k';
     
 $name = 'lanben16_Barer';
     
-// Create connection
-$connection = new PDO("mysql:host=tek.westerdals.no;dbname={$name};port={$port}", $username, $password);
+// Lager en connection til databasen med UTF8 for å få kunne printe ut norske bokstaver
+$connection = new PDO("mysql:host=tek.westerdals.no;dbname={$name};charset=UTF8;port={$port}", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
 ?>
